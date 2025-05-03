2b3794b1e6977087cc83f017537a2433a686330f
Adeola Quadri A. <83679702+adeolaquadri@users.noreply.github.com>
2025-05-03 20:12:32 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
    <footer className="bg-rose-600 text-white py-8 text-center">
-        <p>&copy; {new Date().getFullYear()} Liyah Beauty Salon. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Liyah's Beauty. All rights reserved.</p>
         <div className="mt-4 space-x-4">
           <a href="https://www.instagram.com/hairbyliyah01?igsh=YTRocHRiMGRyZHRk" className="hover:underline">Instagram</a>
           <a href="https://www.facebook.com/" className="hover:underline">Facebook</a>
