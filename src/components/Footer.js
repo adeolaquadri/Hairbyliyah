7466729db4eeb921b0fe9f0802f228cf6ad47a5e
Adeola Quadri <adeolaakorede27@gmail.com>
2025-05-03 18:17:26 +0100

    update footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
    <footer className="bg-rose-600 text-white py-8 text-center">
         <p>&copy; {new Date().getFullYear()} Liyah Beauty Salon. All rights reserved.</p>
         <div className="mt-4 space-x-4">
-          <a href="/" className="hover:underline">Instagram</a>
-          <a href="/" className="hover:underline">Facebook</a>
+          <a href="https://www.instagram.com/hairbyliyah01?igsh=YTRocHRiMGRyZHRk" className="hover:underline">Instagram</a>
+          <a href="https://www.facebook.com/" className="hover:underline">Facebook</a>
           <a href="#contact" className="hover:underline">Contact Us</a>
         </div>
       </footer>
