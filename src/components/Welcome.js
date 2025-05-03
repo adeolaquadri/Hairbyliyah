a3ef8186598380099ceb56dbb74fd411c3f25aff
Adeola Quadri A. <83679702+adeolaquadri@users.noreply.github.com>
2025-05-03 20:17:43 +0100

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ const Welcome = () => {
     <>
       <section className="bg-pink-100 min-h-screen flex items-center justify-center text-center px-4" id="home">
   <div>
-    <h1 className="text-4xl md:text-6xl font-bold text-rose-700">Welcome to Liyah Beauty Salon</h1>
+    <h1 className="text-4xl md:text-6xl font-bold text-rose-700">Welcome to Liyah's Beauty</h1>
     <p className="mt-4 text-lg md:text-xl text-gray-700 max-w-xl mx-auto">
       Where elegance meets excellence. Treat yourself to the beauty and care you deserve.
     </p>
