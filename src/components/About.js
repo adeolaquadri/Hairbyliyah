4430322ef9c7b9b7406c3078123737dffb6f6945
Adeola Quadri A. <83679702+adeolaquadri@users.noreply.github.com>
2025-05-03 20:11:34 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = ()=>{
       <section id="about" className="py-20 bg-white px-6 text-center">
   <h2 className="text-3xl font-bold text-rose-700 mb-4">About Us</h2>
   <p className="text-gray-600 max-w-2xl mx-auto">
-    At Liyah Beauty Salon, we specialize in making you feel beautiful inside and out. From skincare to hairstyling, our professional team is here to pamper you every step of the way.
+    At Liyah's Beauty, we specialize in making you feel beautiful inside and out. From skincare to hairstyling, our professional team is here to pamper you every step of the way.
   </p>
 </section>
 
